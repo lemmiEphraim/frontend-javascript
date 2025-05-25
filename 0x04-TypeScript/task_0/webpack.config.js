1dbbc62ca7eb6e3cd3db1e251eeabebb7762f2c5
lemmiEphraim <lemmiabdissa@gmail.com>
2025-05-26 00:00:50 +0300

    Update webpack.config.js

--- 0x04-TypeScript/task_0/webpack.config.js
+++ 0x04-TypeScript/task_0/webpack.config.js
@@ -1,1 +1,34 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+module.exports = {
+  mode: 'development',
+  entry: './src/main.ts',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
